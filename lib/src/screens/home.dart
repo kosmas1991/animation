132946ae612d8ba9434d01804f36d5a71d88e913
kosmas1991 <kosmasgou@gmail.com>
2021-02-26 20:19:22 +0200

    cat animation flutter

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -9,7 +9,26 @@ class Home extends StatefulWidget{
   }
 }
 
-class HomeState extends State<Home>{
+class HomeState extends State<Home> with TickerProviderStateMixin{
+Animation<double> catAnimation;
+AnimationController catController;
+
+@override
+  void initState() {
+    super.initState();
+    catController = AnimationController(
+      duration: Duration(
+        seconds: 2,
+      ),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +41,14 @@ class HomeState extends State<Home>{
   }
 
   Widget buildAnimation(){
-    return Cat();
+    return AnimatedBuilder(animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+    },
+    child: Cat(),
+    );
   }
 }
